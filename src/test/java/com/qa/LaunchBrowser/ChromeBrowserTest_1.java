24827f92e01c1d1f00f64d2ea33b57e7d61365e2
PavanReddy77 <Pavankrishnan1993@Gmail.com>
2019-12-19 22:43:24 +0530

    Modified

---
+++ src/test/java/com/qa/LaunchBrowser/ChromeBrowserTest_1.java
@@ -0,0 +1,18 @@
+package com.qa.LaunchBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ChromeBrowserTest_1 
+{
+	@Test(priority=1)
+	public void chromeBrowserTest_1() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.quit();
+	}
+}
---
+++ src/test/java/com/qa/LaunchBrowser/TestRunnerFile.java
@@ -0,0 +1,20 @@
+package com.qa.LaunchBrowser;
+
+import org.testng.TestNG;
+
+public class TestRunnerFile 
+{
+	public static TestNG testng;
+	
+	public static void main(String args[])
+	{
+		testng = new TestNG();
+		
+		testng.setTestClasses(new Class[] {ChromeBrowserTest_1.class, ChromeBrowserTest_2.class});
+		testng.run();
+		
+		//Below Code is For ExtentReportListener
+		//ExtentReportListener extentReportListener = new ExtentReportListener();
+		//testng.addListener(extentReportListener);
+	}
+}
