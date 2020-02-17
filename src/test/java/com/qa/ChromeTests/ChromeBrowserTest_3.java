a8e91737767e9d095aa67774e0a29f3bd7a0576c
PavanReddy77 <Pavankrishnan1993@Gmail.com>
2020-02-17 20:12:39 +0530

    Implemented Profile Concept

---
+++ src/test/java/com/qa/ChromeTests/ChromeBrowserTest_3.java
@@ -0,0 +1,31 @@
+package com.qa.ChromeTests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ChromeBrowserTest_3 
+{
+public static WebDriver driver;
+	
+	@Test
+	public void chromeBrowserTest_3() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
+		driver.get("https://www.facebook.com");
+		
+		String title = driver.getTitle();
+		System.out.println("The ChromeBrowserTest_3 Title is ::: " +title);
+		
+		driver.quit();
+	}
+}
